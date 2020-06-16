b161b52fd9a5506ae7be58eda61ac409faab6364
Ho Jin <ycool37@naver.com>
2020-06-16 17:05:49 +0900

    오늘한거 실습

---
+++ src/com/spring/ex02/UserController.java
@@ -0,0 +1,9 @@
+package com.spring.ex02;
+
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+//MVCÁß C
+
+public class UserController extends MultiActionController {
+
+}
